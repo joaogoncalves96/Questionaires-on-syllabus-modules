b27c3f140048e8760492071789f637cbdf219854
Martim <martimcesar@hotmail.com>
2022-05-19 16:54:02 +0100

    remove .next folder

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/addQuestionMultiple":["static\u002Fchunks\u002Fpages\u002FaddQuestionMultiple.js"],sortedPages:["\u002F_app","\u002F_error","\u002FaddQuestionMultiple"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
